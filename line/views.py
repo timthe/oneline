66cff414280e957342aa138feddc44ceba544251
timthe <timthe@gmail.com>
2015-02-07 23:11:11 +0900

    20150207_1111

--- line/views.py
+++ line/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from line.models import Item
 from line.forms import ItemForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     items = Item.objects.all()
@@ -15,3 +16,15 @@ def new_line(request):
         return redirect('/')
     else:
         return render(request, 'newline.html', {'form':form})
+
+def register(request):
+    # http://www.djangobook.com/en/2.0/chapter14.html
+    # https://www.youtube.com/watch?v=xaPHSlTmg1s&list=PLxxA5z-8B2xk4szCgFmgonNcCboyNneMD&index=12
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
